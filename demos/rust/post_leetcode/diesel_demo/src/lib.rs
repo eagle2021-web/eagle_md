845499a6ddb53cf1068c0790b0d37b38580ba14a
eagle2021-web <chenwangying16@163.com>
2023-04-27 22:36:51 +0800

    0427

--- demos/rust/post_leetcode/diesel_demo/src/lib.rs
+++ demos/rust/post_leetcode/diesel_demo/src/lib.rs
@@ -11,7 +11,7 @@ pub mod table_models;
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
+    let database_url = env::var("REMOTE_POST_URL")
         .expect("DATABASE_URL must be set");
     println!("{}", database_url);
     PgConnection::establish(&database_url)
---
+++ demos/rust/post_leetcode/libs/src/collections/my_heap.rs
@@ -0,0 +1,27 @@
+
+#[cfg(test)]
+mod test_heap {
+    use std::collections::BinaryHeap;
+    use std::cmp::Reverse;
+
+    #[test]
+    fn test_small_root_heap() {
+        let mut heap = BinaryHeap::new();
+        heap.push(10);
+        heap.push(30);
+        heap.push(20);
+        assert_eq!(heap.peek().unwrap().clone(), 30);
+        let a = heap.pop().expect("empty heap");
+        assert_eq!(a, 30);
+    }
+
+    #[test]
+    fn test_push() {
+        let mut heap = BinaryHeap::new();
+        heap.push(Reverse(10));
+        heap.push(Reverse(30));
+        heap.push(Reverse(20));
+        assert_eq!(*heap.peek().expect("empty heap"), Reverse(10));
+        assert_eq!(heap.pop().unwrap().0, 10);
+    }
+}
