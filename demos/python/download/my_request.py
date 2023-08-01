d8f69c0d4e0b2b91901eee7e60f2a0b51d1ebd96
eagle2021-web <chenwangying16@163.com>
2023-08-01 22:57:15 +0800

    0801

--- demos/python/download/my_request.py
+++ demos/python/download/my_request.py
@@ -1,4 +1,8 @@
+import hashlib
 import http
+from unittest import TestCase
+
+import http_client
 import unittest
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from http.client import HTTPConnection, HTTPSConnection
@@ -32,30 +36,44 @@ import threading
 
 # 在类的外部定义一个锁
 file_lock = Lock()
+
+
 class Requestor:
     def __init__(self) -> None:
         pass
 
     @classmethod
+    def get_con(cls, url, method, body, headers):
+        print(1)
+        mod = HttpMod.parse_url(url)
+        hc = http_client.HTTPSConnection('www.zenodo.org')
+        hc.request(url=mod.rest_url, method=method, body=body, headers=headers)
+        return hc
+
+    @classmethod
     def get_total_size(cls, url) -> int:
+        mod = HttpMod.parse_url(url)
         response = requests.head(url)
         response.raise_for_status()
-        total_size = int(response.headers.get("content-length", 0))
-        return total_size
+        hc = cls.get_con(url, method='HEAD', body='', headers={})
+        res = hc.getresponse()
+        size = int(res.getheader('content-length'))
+        print(f'size = {size}')
+        return size
     
     @classmethod
-    def check_file_size_integrity(cls, abs_file: Path, totol_size):
-        size = 0
-        if abs_file.exists():
-            size = os.path.getsize(abs_file)
-        else:
-            parent = Path(abs_file.parent)
-            if not parent.exists():
-                parent.mkdir(parents=True)
-            with open(abs_file, 'ab') as f:
-                pass
-        return size == total_size
-
+    def cnt_md5(cls, abs_path):
+        md5 = hashlib.md5()
+        with open(abs_path, 'rb') as f:
+            while True:
+                data = f.read(4096)
+                if not data:
+                    break
+                md5.update(data)
+        md5_str = md5.hexdigest()
+        print(f'md5_str = {md5_str}')
+        return md5_str
+    
     @classmethod
     def download(cls, url: str, rel_dir: str, max_thread=4):
         mod = HttpMod.parse_url(url)
@@ -63,21 +81,21 @@ class Requestor:
         abs_file = Path(local_mod.abs_file_path)
         total_size = cls.get_total_size(url=url)
         FileUtils.create_file(abs_file=abs_file, total_size=total_size)
-        divided_list = Partition.get_div_list(total_size=total_size,  max_thread=max_thread)
+        divided_list = Partition.get_div_list(total_size=total_size, max_thread=max_thread)
         if divided_list is None:
             return
         progress = Progress(TextColumn("[progress.description]{task.description}"),
-                    SpinnerColumn(),
-                    BarColumn(),
-                    # FileSizeColumn(),
-                    # TotalFileSizeColumn(),
-                    DownloadColumn(),
-                    # TransferSpeedColumn(),
-                    TransferSpeedColumn(),
-                    TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
-                    TimeRemainingColumn(),
-                    TimeElapsedColumn(),
-                    refresh_per_second=0.3)
+                            SpinnerColumn(),
+                            BarColumn(),
+                            # FileSizeColumn(),
+                            # TotalFileSizeColumn(),
+                            DownloadColumn(),
+                            # TransferSpeedColumn(),
+                            TransferSpeedColumn(),
+                            TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+                            TimeRemainingColumn(),
+                            TimeElapsedColumn(),
+                            refresh_per_second=0.3)
 
         # 添加一个任务，指定任务总数
         task_list = []
@@ -100,47 +118,55 @@ class Requestor:
                 futures.append(p.submit(cls.range_download, url, local_mod.abs_file_path, s_pos, e_pos,
                                         progress, cur_task, queue, idx))
             # 等待所有任务执行完毕
-            print('等待所有任务执行完毕')
             print(len(futures))
             as_completed(futures)
-            print('所有任务执行完毕')
-        print('here')            
+        print('here')
         thread.join()
         progress.stop()
+        cls.cnt_md5(abs_path=abs_file)
         sleep(1)
 
     @classmethod
     def range_download(cls, url, abs_path, s_pos, e_pos, progress: Progress, task, queue: Queue, idx: int):
         begin = s_pos
+        abs_file = Path(abs_path)
+        offset = s_pos
+        hc, res = None, None
         while True:
             retry = 0
             try:
                 headers = {f"Range": f"bytes={s_pos}-{e_pos}"}
-                # response = requests.get(url, stream=True, headers=headers)
-                # response.raise_for_status()
-                abs_file = Path(abs_path)
                 print(f'下载从{s_pos}到{e_pos}')
                 offset = s_pos
-                with requests.get(url, stream=True, headers=headers) as response:
-                    for chunk in response.iter_content(chunk_size=1024 * 256):
-                        if not chunk:
-                            break
-                        progress.advance(task, advance=len(chunk))         
+                hc = cls.get_con(url, method='GET', body='', headers=headers)
+                res = hc.getresponse()
+                chunk_size = 1024 * 256
+                while True:
+                    chunk = res.read(chunk_size)
+                    if chunk:
+                        progress.advance(task, advance=len(chunk))
                         queue.put((None, chunk, len(chunk), offset, idx))
                         offset += len(chunk)
+                    else:
+                        break
                 queue.put((True, None, None, None, None))
-                # response.close()
                 break
             except Exception as e:
                 print(e)
                 print(f"异常{str(abs_file)}分片：{s_pos}-{e_pos}")
                 print(f'下一个开始于{offset}')
-                progress.update(task, completed=offset - begin)  
+                progress.update(task, completed=offset - begin)
                 s_pos = offset
                 retry += 1
                 if retry == 4:
                     break
                 sleep(5)
+            finally:
+                if res:
+                    res.close()
+                if hc:
+                    hc.close()
+
     @classmethod
     def write_file(cls, abs_file: Path, queue: Queue, end_cnt: int):
         done_count = 0
@@ -155,8 +181,8 @@ class Requestor:
                 else:
                     f.seek(offset)
                     f.write(data)
-if __name__ == '__main__':
-    url = "https://www.zenodo.org/api/files/fafb41f4-4679-4ade-8ec6-1e48ecd43072/mentions.zip"
-    url = 'https://www.zenodo.org/record/7834392/files/full_dataset.tsv.gz.part-aa?download=1'
-    total_size = Requestor.download(url=url, rel_dir='a19', max_thread=10)
+
+
+
+
 
