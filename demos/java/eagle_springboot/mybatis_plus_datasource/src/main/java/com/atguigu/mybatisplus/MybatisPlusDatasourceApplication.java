d91782a61e41c3f8450f914e9bdf3f1ba25a23b6
eagle2021-web <chenwangying16@163.com>
2023-08-07 23:21:03 +0800

    0807

--- demos/java/eagle_springboot/mybatis_plus_datasource/src/main/java/com/atguigu/mybatisplus/MybatisPlusDatasourceApplication.java
+++ demos/java/eagle_springboot/mybatis_plus_datasource/src/main/java/com/atguigu/mybatisplus/MybatisPlusDatasourceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.atguigu.mybatisplus.mapper")
 public class MybatisPlusDatasourceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusDatasourceApplication.class, args);
     }
-
 }
