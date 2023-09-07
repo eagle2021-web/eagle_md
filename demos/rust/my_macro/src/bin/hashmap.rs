89bfc246d28479eb6945348adf7c99ad669f8aeb
eagle2021-web <chenwangying16@163.com>
2023-09-08 01:05:52 +0800

    0908

---
+++ demos/rust/my_macro/src/bin/hashmap.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+use std::mem::{size_of, size_of_val};
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+        let mut map = ::std::collections::HashMap::new();
+        $( map.insert($key, $val); )*
+        map
+    }}
+}
+
+fn main() {
+    let s = "dasf";
+    println!("P{}", s.len());
+    println!("P{:p}", &s);
+    println!("P{}", std::mem::size_of_val(&s));
+    println!("P{}", std::mem::size_of_val(s));
+}
--- demos/rust/my_sz/base/src/bin/expand.rs
+++ demos/rust/my_sz/base/src/bin/expand.rs
@@ -4,6 +4,8 @@ fn main() {
     // cargo expand --bin expand
     let sum = vec![11, 12];
     let a = Box::new(232);
-    let sum = <[_]>::into_vec(alloc::boxed::Box::new([11, 12]));
+    let sum = <[_]>::into_vec(::alloc::boxed::Box::new([11, 12]));
     let sum = Box::new([1212, 12]).to_vec();
+    let sum = vec![10; 20];
+    let sum = ::alloc::vec::from_elem(10, 20);
 }
